86536aeb95ba14897e2365cfe1ead32da7df6144
IanO-B4 <ianhome0712@gmail.com>
2026-09-09 18:07:23 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String.HappyBday=newstring("Happy Birthday, ");
+		String.ToYou=newstring("to you.");
+		String.DearName=newstring("dear proper name of somebody in the class");
+		System.out.println(HappyBday+ToYou);
+		System.out.println(HappyBday+ToYou);
+		System.out.println(HappyBday+DearName);
+		System.out.print(HappyBday+ToYou);
 	}
 }
