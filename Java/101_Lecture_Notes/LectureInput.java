feaf863806e3f0b55160923a15000deb26053340
IanO-B4 <ianhome0712@gmail.com>
2026-09-15 17:08:10 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,21 @@
 /*
     Lecture note example - Input!!
 */
-
+import java.util.Scanner;
 class LectureInput{
     public static void main(String args[]) {
         // Your Code Goes here!
+    Scanner sc = new Scanner(System.in);
+    System.out.println("🍕 Welcome to Mama Mia Pizzeria 🍕");
+    System.out.println("Here's our menu:");
+    double CpPrice = 1.00;
+    double PpPrice = 1.50;
+    double DrinkPrice = 0.99;
+    System.out.println("1. Cheese Slice - $"+CpPrice);
+    System.out.println("2. Pepperoni Slice - $"+PpPrice);
+    System.out.println("3. Fountain Drink - $"+DrinkPrice);
+    System.out.println("");
+    System.out.println("How many cheese slices would you like?")
+    int CpQuant
 	}
 }
